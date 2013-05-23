9423928afcf04cef48c34a665af5bb7eb9f8c27c
Pawel W <elendil@konto.pl>
2013-05-23 18:29:21 -0400

    definition of Global types

---
+++ Global/GlobalTypes.h
@@ -0,0 +1,19 @@
+#ifndef GLOBALTYPES_H
+#define GLOBALTYPES_H
+#include <list>
+#include <vector>
+#include <string>
+
+typedef unsigned char 	UInt8;
+typedef unsigned short 	UInt16;
+typedef unsigned int 		UInt32;
+typedef char						Int8;
+typedef short						Int16;
+typedef int							Int32;
+
+typedef float						Real32;
+typedef double					Real64;
+
+typedef std::list<std::string> tStringList;
+typedef std::vector<std::string> tStringVector;
+#endif
