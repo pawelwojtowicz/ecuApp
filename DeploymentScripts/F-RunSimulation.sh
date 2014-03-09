e7ff093e96c15c4e63ca1a4198ba8d52755de10d
Pawel W <elendil@konto.pl>
2014-03-08 20:25:38 -0500

    some extensions, corrections, but the the QEMU does not start

--- DeploymentScripts/F-RunSimulation.sh
+++ DeploymentScripts/F-RunSimulation.sh
@@ -1,9 +1,9 @@
-CURR_DIR=$(pwd)
+#!/bin/bash
+source deploymentPaths
 
-KERNEL_DIR=linux-3.3.3
-
-LINUX_IMG_PATH=$CURR_DIR/$KERNEL_DIR/arch/arm/boot/zImage
+LINUX_IMG_PATH=$THIRD_PARTY_COMPONENTS/$KERNEL_VERSION/arch/arm/boot/zImage
 
 echo "starting qemu with "$LINUX_IMG_PATH
 
-qemu-system-arm -m 128 -M versatilepb -kernel $LINUX_IMG_PATH -nographic -append "console=ttyAMA0,115200 initrd=dev/ram0 init=init"
+
+qemu-system-arm -m 128 -M versatilepb -kernel $LINUX_IMG_PATH -nographic -append "console=ttyAMA0,115200 root=dev/ram"
