80dc822a3e285faf6fce0246f9d476e63637803b
Pawel W <elendil@konto.pl>
2014-09-13 00:08:14 -0400

    GMOCK integrated. WOW

--- DevEnv/prepareTestEnvironment.sh
+++ DevEnv/prepareTestEnvironment.sh
@@ -5,4 +5,6 @@ export CONFIG_DIR=$ROOT/Configuration
 export LOG_DIR=$ROOT/var/log
 export TMP_DIR=$ROOT/tmp
 unzip ../3rdParty/gmock-1.7.0.zip -d ../3rdParty
-export GOOGLE_TEST=$ROOT/3rdParty/gmock-1.7.0/gtest
+export GMOCK_DIR=$ROOT/3rdParty/gmock-1.7.0
+export GOOGLE_TEST=$GMOCK_DIR/gtest
+
--- UnitTestSrc/Runtime/CTest_Messenger.cpp
+++ UnitTestSrc/Runtime/CTest_Messenger.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>  // Brings in Google Mock.
 #include <Runtime/CMessenger.h>
 #include <Runtime/CMessage.h>
 #include <Runtime/ISubscriber.h>
@@ -83,3 +84,79 @@ TEST( CMessenger,ConnectQeue_RefCount )
   
 	messenger.Shutdown();
 }
+
+
+ 
+ using ::testing::AtLeast;
+ using ::testing::Return;
+ 
+ class CMokowana
+ {
+ public
+ :
+     virtual int PierwszaMetoda() = 0;
+     virtual float DrugaMetoda( int tmp ) = 0;
+     virtual float TrzeciaMetodaConst( int tmp ) const = 0;
+ 
+ };
+ 
+ class MockMokowanej: public CMokowana
+ {
+ public:
+     MOCK_METHOD0(PierwszaMetoda, int() );
+     MOCK_METHOD1(DrugaMetoda,float( int tmp ) );
+     MOCK_CONST_METHOD1(TrzeciaMetodaConst,float( int tmp ) );
+ };
+ 
+ 
+ class Testowana
+ {
+ public:
+     Testowana( CMokowana& referencja ):m_ref(referencja) {}
+ 
+     int Metoda1()
+     {
+         return m_ref.PierwszaMetoda();
+     }
+ 
+     float Metoda2( int tmp )
+     {
+         return m_ref.DrugaMetoda(tmp);
+     }
+ 
+     float Metoda3( int tmp )
+     {
+         return m_ref.TrzeciaMetodaConst( tmp );
+     }
+ 
+ private:
+     CMokowana& m_ref;
+ };
+ 
+ TEST(PainterTest, CanDrawSomething)
+ {
+   MockMokowanej mok;                          // #2
+   EXPECT_CALL(mok, PierwszaMetoda())              // #3
+       .Times(3).WillOnce(Return(0)).WillRepeatedly(Return(1));
+ 
+   Testowana tmp(mok);// #4
+   EXPECT_FALSE(tmp.Metoda1());
+   EXPECT_TRUE(tmp.Metoda1());
+ 
+   EXPECT_TRUE( tmp.Metoda1() );
+ }
+ 
+ TEST(PainterTest, Argumentowana)
+ {
+   MockMokowanej mok;                          // #2
+   EXPECT_CALL(mok, DrugaMetoda(123))              // #3
+       .Times(1).WillOnce(Return(13));
+ 
+   Testowana tmp(mok);// #4
+ 
+   ASSERT_FLOAT_EQ(13,tmp.Metoda2(123));
+ }
+ 
+ 
+ 
+
