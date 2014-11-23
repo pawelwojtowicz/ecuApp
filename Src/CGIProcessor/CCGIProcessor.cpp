fdf8ef7a54ea1ccb602a9c2d7625253a8281ba7a
Pawel W <elendil@konto.pl>
2014-11-22 19:47:48 -0500

    CGI Processor uses the JSON now, to return the Command execution result

--- Src/CGIProcessor/CCGIProcessor.cpp
+++ Src/CGIProcessor/CCGIProcessor.cpp
@@ -1,6 +1,7 @@
 #include "CCGIProcessor.h"
 #include "CGIProcessorConst.h"
 #include "CCommandFactory.h"
+#include "JSONLib/json.h"
 
 
 #include <stdio.h>
@@ -30,19 +31,32 @@ void CCGIProcessor::Initialize()
 Int32 CCGIProcessor::Run()
 {
   // there has to be written those 2 lines to be able to see something on the
-  printf("Content-type: text/plain\r\n");
+  printf("Content-type: application/json\r\n");
   printf("\r\n");
+  
+  // prepare the structure for returning the JSON data
+  json::Object requestResponse;
 
   std::string commandName = m_environment.GetVariable(s_const_sv_commandName);
-  printf( "Dostalem komende %s\n", commandName.c_str() );
+  
   
   ICommand* pCommand = m_commandFactory.GetCommand(commandName);
   if ( 0 != pCommand )
   {
     bool retVal(pCommand->Execute(m_environment));
 
-    printf( "znalazlem i wykonalem komende %d\n", retVal);
+    json::Object commandExecutionResults;
+    
+    commandExecutionResults["CommandName"] = commandName.c_str();
+    commandExecutionResults["Result"] = retVal;
+    
+    requestResponse["CommandExecution"]=commandExecutionResults;
   }
+  
+  // serialize the JSON into the string, and send it to the std output.
+  std::string jsonInString(json::Serialize(requestResponse));
+  printf("%s", jsonInString.c_str() );
+  
   return 0;
 }
 
