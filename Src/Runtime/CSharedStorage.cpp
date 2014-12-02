df1bc0ac83999ce26b84047b05edf7de54c92d9e
Pawel W <elendil@konto.pl>
2014-12-02 16:15:55 -0500

    Shared memory reorganized. Further extensions will be damn easier

--- Src/Runtime/CSharedStorage.cpp
+++ Src/Runtime/CSharedStorage.cpp
@@ -1,28 +1,13 @@
 #include "CSharedStorage.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/mman.h>
-#include <sys/sem.h>
-#include <sys/ipc.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
-#include <errno.h>
 #include <UCL/CSerializable.h>
 
 
 namespace Runtime
 {
 
-CSharedStorage::CSharedStorage(const std::string& storageName, const size_t size, const bool owner )
-: m_storageName(storageName)
-, m_size(size)
-, m_owner(owner)
-, m_hSharedMemoryHandle(-1)
-, m_pMemory(0)
-, m_lockHandle(SEM_FAILED)
+CSharedStorage::CSharedStorage()
+: m_sharedMemory()
+, m_lockFacility()
 {
 }
 
@@ -31,34 +16,15 @@ CSharedStorage::~CSharedStorage()
 {
 }
 
-bool CSharedStorage::Initialize()
+bool CSharedStorage::Initialize(const std::string& storageName, const size_t size, const bool owner)
 {
 	bool retVal(false);
-
-	int flags = m_owner?(O_RDWR|O_CREAT):(O_RDONLY|O_CREAT);
-
-	m_hSharedMemoryHandle = shm_open( m_storageName.c_str(), flags, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP| S_IROTH | S_IWOTH);
-	if ( m_hSharedMemoryHandle != -1 )
+	
+	retVal = m_sharedMemory.Open(storageName, size, owner);
+	
+	if (retVal)
 	{
-		if (ftruncate(m_hSharedMemoryHandle, m_size) != -1 )
-		{
-			m_pMemory = (char*)mmap(0, m_size, (m_owner?PROT_WRITE:PROT_READ), MAP_SHARED, m_hSharedMemoryHandle, 0 );
-
-			retVal = ( MAP_FAILED != m_pMemory );
-
-			if (retVal)
-			{
-				m_lockHandle = sem_open(m_storageName.c_str(), O_CREAT , ( S_IRWXO | S_IRWXG | S_IRWXU) , 1 );
-				if (SEM_FAILED != m_lockHandle )
-				{
-					retVal = true;
-				}
-				else
-				{
-					printf("Failed sem_open [%s]\n", strerror(errno));
-				}
-			}
-		}
+		retVal = m_lockFacility.Initialize(storageName, 1);
 	}
 
 	return retVal; 
@@ -67,45 +33,38 @@ bool CSharedStorage::Initialize()
 bool CSharedStorage::Shutdown()
 {
 	bool retVal(false);
-
-
-	if (-1 != shm_unlink( m_storageName.c_str() ) )
-	{
-		retVal = true;
-	}
 	
-	sem_close(m_lockHandle);
+	m_sharedMemory.Close();
+
+	m_lockFacility.Shutdown();
 
 	return retVal; 
-}
-
-void CSharedStorage::LockStorage()
-{
-	sem_wait(m_lockHandle);
-}
-
-void CSharedStorage::UnlockStorage()
-{
-	sem_post(m_lockHandle);
 }
 
 bool CSharedStorage::SetData( UCL::CSerializable& serializableData)
 {
 	bool retVal(false);
-	LockStorage();
-	retVal = serializableData.Serialize(m_pMemory,m_size);
-	UnlockStorage();
+	m_lockFacility.Lock();
+	if (m_sharedMemory.IsValid() )
+	{
+		retVal = serializableData.Serialize(m_sharedMemory.GetMemoryPointer(),m_sharedMemory.GetMemorySize());
+	}
+	m_lockFacility.Unlock();	
 	return retVal;
 }
 
 bool  CSharedStorage::GetData( UCL::CSerializable& serializableData)
 {
 	bool retVal(false);
-	LockStorage();
-	retVal = serializableData.Deserialize(m_pMemory,m_size);
-	UnlockStorage();
-	return retVal;
+	m_lockFacility.Lock();
 
+	if (m_sharedMemory.IsValid() )
+	{
+		retVal = serializableData.Deserialize(m_sharedMemory.GetMemoryPointer(),m_sharedMemory.GetMemorySize());
+	}
+	
+	m_lockFacility.Unlock();	
+	return retVal;
 }
 
 
