dada76424a10e7e6db867cea615d43ccde46aa3d
Pawel W <elendil@konto.pl>
2015-08-15 15:57:39 -0400

    GoogleHash integrated with the EcuApp

--- Src/Global/GlobalTypes.h
+++ Src/Global/GlobalTypes.h
@@ -1,5 +1,6 @@
 #ifndef GLOBALTYPES_H
 #define GLOBALTYPES_H
+#include <stdint.h>
 #include <list>
 #include <vector>
 #include <map>
@@ -11,6 +12,8 @@ typedef unsigned int 		UInt32;
 typedef char						Int8;
 typedef short						Int16;
 typedef int							Int32;
+typedef int64_t					Int64;
+typedef uint64_t				UInt64;
 
 typedef float						Real32;
 typedef double					Real64;
